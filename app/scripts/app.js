2535fa3d87f2416bf92f25e1fbae9a867ccfb73b
julien <gravatar@gravatar.com>
2014-06-26 19:31:42 +0200

    Mise à jour de l'interface

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,10 +1,14 @@
-Vue.component('multi', { template: vTemplate('partials/multi-devices.html') })
-Vue.component('settings', { template: vTemplate('partials/settings.html') })
-Vue.component('fiche', { template: vTemplate('partials/device.html') })
 window.snmpDevices = {};
 window.syslogs = [];
 
+var gui = require('nw.gui');
+var win = gui.Window.get();
 
+
+
+Vue.component('multi', { template: vTemplate('partials/multi-devices.html') })
+Vue.component('settings', { template: vTemplate('partials/settings.html') })
+Vue.component('fiche', { template: vTemplate('partials/device.html') })
 
 var app = new Vue({
 	el: '#app',
@@ -24,5 +28,18 @@ var app = new Vue({
 
 		}
 	},
+    methods: {
+        freeStorage: function(device) {
+            var free = 0;
+            for (var i = device.mountedDisks.length - 1; i >= 0; i--) {
+                free += device.mountedDisks[i].getDiskFree();
+            };
+            return device.mountedDisks.length > 0 ? free : null;
+        },
+        freeStorageMessage: function(device) {
+            var free = this.freeStorage(device);
+            return free == null ? 'inconnu' : free + 'Go disponnible';
+        }
+    },
 	created: function(){initInfos()}
 })
