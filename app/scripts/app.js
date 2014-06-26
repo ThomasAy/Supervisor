355e3a0c5923a43f25832937e62564a82e4d7a5b
julien <gravatar@gravatar.com>
2014-06-26 21:58:56 +0200

    Interface monitoring

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,7 +4,14 @@ window.syslogs = [];
 var gui = require('nw.gui');
 var win = gui.Window.get();
 
+document.getElementById('close').addEventListener('click', function(){
+	debugger;
+	win.close();
+}, false)
 
+document.getElementById('mini').addEventListener('click', function(){
+	win.minimize();
+}, false)
 
 Vue.component('multi', { template: vTemplate('partials/multi-devices.html') })
 Vue.component('settings', { template: vTemplate('partials/settings.html') })
@@ -34,11 +41,11 @@ var app = new Vue({
             for (var i = device.mountedDisks.length - 1; i >= 0; i--) {
                 free += device.mountedDisks[i].getDiskFree();
             };
-            return device.mountedDisks.length > 0 ? free : null;
+            return device.mountedDisks.length > 0 ? Math.round(free) : null;
         },
         freeStorageMessage: function(device) {
             var free = this.freeStorage(device);
-            return free == null ? 'inconnu' : free + 'Go disponnible';
+            return free == null ? 'non disponnible' : free + 'Go disponnible';
         }
     },
 	created: function(){initInfos()}
--- app/scripts/routes.js
+++ app/scripts/routes.js
@@ -1,6 +1,6 @@
 var router = new Router({ 
-    '/':        function() { updateInfos(); app.currentView = 'multi'; }, 
-    '/settings/': function() { app.currentView = 'settings'; }, 
+    '/':        function() { app.deviceId = -1; updateInfos(); app.currentView = 'multi'; }, 
+    '/settings/': function() { app.deviceId = -1; app.currentView = 'settings'; }, 
     '/device/:id': function(id) { app.deviceId = id ; app.currentView = 'fiche'; getLogFromIp(app.devices[id].ip)}
 }); 
 router.init();
